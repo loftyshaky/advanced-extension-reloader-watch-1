df4e6a7876a609f10f0cc33571826ada28e5f869
loftyshaky <loftyshaky@gmail.com>
2020-08-30 02:43:59 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require('fs-extra');
+const _ = require('lodash');
+const yargs = require('yargs');
+const { greenBright, redBright } = require('colorette');
+
+const { Reload } = require('extension-reloader-watch-2');
+const { isNil } = require('lodash');
+
+const options = yargs
+    .usage('Usage: -c <config>')
+    .option('c',
+        {
+            alias: 'config',
+            describe: 'Absolute path to json config file.',
+            type: 'string',
+            demandOption: true,
+        })
+    .argv;
+
+const config = fs.readJSONSync(options.config);
+const obj = {
+    1: {},
+    2: {},
+};
+
+const resolve_property = (obj_no, config_key) => {
+    if (!_.isNil(config) && !_.isNil(config[config_key])) {
+        obj[obj_no][config_key] = config[config_key];
+    }
+};
+
+resolve_property(
+    1,
+    'watch_dirs',
+);
+
+if (isNil(obj[1].watch_dirs)) {
+    // eslint-disable-next-line no-console
+    console.log(redBright('Config json doesn\'t contain watch_dirs property.'));
+} else {
+    resolve_property(
+        1,
+        'hard_dirs',
+    );
+    resolve_property(
+        2,
+        'hard',
+    );
+    resolve_property(
+        2,
+        'all_tabs',
+    );
+
+    const reload = new Reload(obj[1]);
+
+    reload.watch(
+        {
+            callback: () => reload.reload(obj[2]),
+        },
+    );
+
+    // eslint-disable-next-line no-console
+    console.log(greenBright('Watching...'));
+}
