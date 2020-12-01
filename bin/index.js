c864d789316d53432e2f0f379c18b9e0b8105892
loftyshaky <loftyshaky@gmail.com>
2020-12-01 13:10:46 +1000

    feat: update how page reloads

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,11 @@
 const fs = require('fs-extra');
 const { isNil } = require('lodash');
 const yargs = require('yargs');
-const { greenBright, redBright } = require('colorette');
+const {
+    greenBright,
+    redBright,
+    blueBright,
+} = require('colorette');
 
 const { Reload } = require('extension-reloader-watch-2');
 
@@ -17,10 +21,10 @@ const options = yargs
         })
     .argv;
 
-const config = fs.readJSONSync(options.config);
-const obj = {
+let config = fs.readJSONSync(options.config);
+const config_obj = {
+    0: {},
     1: {},
-    2: {},
 };
 
 const resolve_property = (obj_no, config_key) => {
@@ -28,44 +32,64 @@ const resolve_property = (obj_no, config_key) => {
         !isNil(config)
         && !isNil(config[config_key])
     ) {
-        obj[obj_no][config_key] = config[config_key];
+        config_obj[obj_no][config_key] = config[config_key];
     }
 };
 
 resolve_property(
-    1,
+    0,
     'port',
 );
 resolve_property(
-    1,
-    'watch_dirs',
+    0,
+    'watch_paths',
 );
 
-if (isNil(obj[1].watch_dirs)) {
-    // eslint-disable-next-line no-console
-    console.log(redBright('Config json doesn\'t contain watch_dirs property.'));
-} else {
-    resolve_property(
-        1,
-        'hard_dirs',
-    );
-    resolve_property(
-        2,
-        'hard',
-    );
-    resolve_property(
-        2,
-        'all_tabs',
-    );
+const reload = new Reload(config_obj[0]);
 
-    const reload = new Reload(obj[1]);
+reload.watch(
+    {
+        callback: () => {
+            if (isNil(config_obj[0].watch_paths)) {
+                // eslint-disable-next-line no-console
+                console.log(redBright('Config json doesn\'t contain watch_paths property.'));
+            } else {
+                config = fs.readJSONSync(options.config);
+                config_obj[1] = {};
 
-    reload.watch(
-        {
-            callback: () => reload.reload(obj[2]),
+                resolve_property(
+                    1,
+                    'hard',
+                );
+                resolve_property(
+                    1,
+                    'all_tabs',
+                );
+                resolve_property(
+                    1,
+                    'hard_paths',
+                );
+                resolve_property(
+                    1,
+                    'soft_paths',
+                );
+                resolve_property(
+                    1,
+                    'all_tabs_paths',
+                );
+                resolve_property(
+                    1,
+                    'one_tab_paths',
+                );
+
+                reload.reload(config_obj[1]);
+
+                // eslint-disable-next-line no-console
+                console.log(blueBright(`Reloaded extension on ${new Date().toLocaleTimeString()}`));
+            }
         },
-    );
+    },
+);
 
-    // eslint-disable-next-line no-console
-    console.log(greenBright('Watching...'));
-}
+// eslint-disable-next-line no-console
+console.log(greenBright('Watching...'));
